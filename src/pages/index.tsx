9195f022ffbd6ad17406e09b24cb9d0c67abef04
Brandon Dong <gasingdong@gmail.com>
2020-06-03 14:53:52 -0700

    add axios call to get pokemon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import 'normalize.css';
 import './index.scss';
 
 const Home: React.FC = () => {
-  return <h1>Hello Gatsby-Typescript!</h1>;
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemon = async (): Promise<void> => {
+      const result = await axios.get('http://localhost:5000/api/pokemon');
+      setPokemon(result.data.results);
+    };
+    fetchPokemon();
+  }, []);
+
+  return (
+    <>
+      {pokemon.map((mon: BasicPokemon) => (
+        <h1 key={mon.name}>{mon.name}</h1>
+      ))}
+    </>
+  );
+};
+
+type BasicPokemon = {
+  name: string;
+  url: string;
 };
 
 export default Home;
